f7bba5a3d3bd591e18f897b424af99630c453a7d
yves <yves@empreintedigitale.fr>
2021-03-07 12:36:03 +0100

    ...

--- src/static/js/ace2_inner.js
+++ src/static/js/ace2_inner.js
@@ -195,7 +195,7 @@ function Ace2Inner(editorInfo) {
       window.console.log(error);
       window.console.log(atext);
       var tmpTxt='\n';
-      tmpTxt=tmpTxt.join(atext.text);
+      tmpTxt.concat(atext.text,tmpTxt);
       
       atext.text=tmpTxt;
       setDocText(atext);
